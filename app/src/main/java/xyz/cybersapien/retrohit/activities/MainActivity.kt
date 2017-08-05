606de2f5537ac31282e3beb881d6a8bac177f125
Aditya Aggarwal <aditya@cybersapien.xyz>
2017-08-05 23:34:47 +0530

    Generalized Request method to handle all kinds of requests

--- app/src/main/java/xyz/cybersapien/retrohit/activities/MainActivity.kt
+++ app/src/main/java/xyz/cybersapien/retrohit/activities/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import okhttp3.Call
@@ -11,13 +14,17 @@ import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import xyz.cybersapien.retrohit.R
-import xyz.cybersapien.retrohit.network.buildGETRequest
+import xyz.cybersapien.retrohit.network.buildRequest
+import xyz.cybersapien.retrohit.network.isValidURL
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    val LOG_TAG = "MainActivity"
     lateinit var httpClient: OkHttpClient
+    lateinit var methodsAdapter: ArrayAdapter<CharSequence>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,28 +32,31 @@ class MainActivity : AppCompatActivity() {
 
         httpClient = clientBuilder()
 
-        fab.setOnClickListener { _ ->
-            var url = url_edit_text.text.toString()
-            if (!url.startsWith("http://") && !url.startsWith("https://"))
-                url = "http://" + url
-            val getRequest = buildGETRequest(url)
-            httpClient.newCall(getRequest)
-                    .enqueue(object : Callback {
-                        override fun onFailure(call: Call?, e: IOException?) {
-                            val handler = Handler(Looper.getMainLooper())
-                            handler.post({
-                                result_textview.text = "Error!\n${call.toString()}\n${e.toString()}"
-                            })
-                        }
+        methodsAdapter = ArrayAdapter.createFromResource(this, R.array.methods_array, android.R.layout.simple_spinner_item)
+        methodsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-                        override fun onResponse(call: Call?, response: Response) {
-                            val handler = Handler(Looper.getMainLooper())
-                            val responseBody = response.body()
-                            handler.post({
-                                result_textview.text = responseBody?.string()
-                            })
-                        }
-                    })
+        methods_spinner.adapter = methodsAdapter
+
+        fab.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            fab.id -> {
+                var url = url_edit_text.text.toString()
+                if (!url.startsWith("http://") && !url.startsWith("https://"))
+                    url = "http://" + url
+                if (isValidURL(url)) {
+                    performRequest(url)
+                } else {
+                    url_edit_text.error = "Invalid URL!"
+                }
+                Log.d(LOG_TAG, methods_spinner.selectedItem.toString())
+            }
+            else -> {
+                // Do Nothing
+            }
         }
     }
 
@@ -59,6 +69,36 @@ class MainActivity : AppCompatActivity() {
         // TODO: Add Retry param
         clientBuilder.retryOnConnectionFailure(true)
         return clientBuilder.build()
+    }
+
+    fun performRequest(url: String): Unit {
+
+        val method = methods_spinner.selectedItem as String
+
+        val getRequest = buildRequest(url, method)
+
+        httpClient.newCall(getRequest)
+                .enqueue(object : Callback {
+                    override fun onFailure(call: Call?, e: IOException?) {
+                        val handler = Handler(Looper.getMainLooper())
+                        handler.post({
+                            result_textview.text = getString(R.string.error_string, e.toString())
+                        })
+                    }
+
+                    override fun onResponse(call: Call?, response: Response) {
+                        val handler = Handler(Looper.getMainLooper())
+                        val responseBody = response.body()
+                        val headers = response.headers()
+                        val resString = response.toString()
+                        val resBodyString = responseBody?.string()
+                        handler.post({
+                            Log.d(LOG_TAG, resString)
+                            Log.d(LOG_TAG, headers.toString())
+                            result_textview.text = resBodyString
+                        })
+                    }
+                })
     }
 
 }
--- app/src/main/java/xyz/cybersapien/retrohit/network/RetroClient.kt
+++ app/src/main/java/xyz/cybersapien/retrohit/network/RetroClient.kt
@@ -4,19 +4,23 @@ import okhttp3.Headers
 import okhttp3.Request
 import okhttp3.RequestBody
 
-fun buildGETRequest(
+fun buildRequest(
         URL: String,
+        method: String,
         headers: Headers? = null,
         requestBody: RequestBody? = null
 ): Request {
 
-    val requestBuilder = Request.Builder()
-            .url(URL)
+    val requestBuilder = Request.Builder().url(URL)
 
     if (headers != null)
         requestBuilder.headers(headers)
     if (requestBody != null)
-        requestBuilder.method("GET", requestBody)
+        requestBuilder.method(method, requestBody)
 
     return requestBuilder.build()
+}
+
+fun isValidURL(url: String): Boolean {
+    return url.matches(Regex("^(http[s]?://)([^:/\\s]+)(/.*)*$"))
 }
